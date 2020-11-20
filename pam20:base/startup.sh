094ae49fd78c3b0fdf1b225e819111f7420b1be3
Mark Stanley Santiago Burgos <mssb71@gmail.com>
2020-11-20 10:02:52 +0100

    pam20

--- pam20:base/startup.sh
+++ pam20:base/startup.sh
@@ -1,4 +1,20 @@
 #! /bin/bash
+# ASIX M06 Mark Santiago
+# Install users and login.defs
+#--------------------------------------
 
-/opt/docker/install.sh
+# Put login.defs with CHFN RESTRICTION no 
+rm /etc/login.defs
+cp /opt/docker/login.defs /etc/login.defs
+# Add users
+
+users="pere pau anna"
+
+for user in $users
+  do
+    useradd $user && echo "OK $user" 
+    # Add passwords
+    echo "$user" | passwd --stdin $user &> /dev/null && echo "OK password $user"
+  done
+#----
 /bin/bash
