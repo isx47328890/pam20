2e840d698f523c2ec2580d2a5c7cc3cdf57f3057
Mark Stanley Santiago Burgos <mssb71@gmail.com>
2020-11-30 13:13:32 +0100

    authconfig

---
+++ pam20:auth/auth.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+authconfig --enableshadow --enablelocauthorize \
+  	   --enableldap \
+  	   --enableldapauth \
+  	   --ldapserver='ldap.edt.org' \
+  	   --ldapbase='dc=edt,dc=org' \
+	   --enablemkhomedir \
+  	   --updateall
